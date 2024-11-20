4b7caabdfaeb75833671dee3dec6176777bdc2b6
Adrian Gidaro <adriangidaro@gmail.com>
2024-11-20 19:56:27 +1100

    Mid class save

---
+++ if_else.py
@@ -0,0 +1,27 @@
+# is_raining = True
+
+# if is_raining:
+#     print('Bring Umbrella')
+# else:
+#     print('Sunny Days')
+# print('The end!')
+
+# Else if
+# >80 -> HD
+# 70-79 -> D
+# 60-69 -> C
+# 50-59 -> P
+# <50 -> F
+
+marks = 51
+
+if marks >= 80:
+    print('HD')
+elif marks >= 70:
+    print('D')
+elif marks >= 60:
+    print('C')
+elif marks >= 50:
+    print('P')
+elif marks < 50:
+    print('F')
