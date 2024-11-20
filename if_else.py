938bf818e52e227f21337b7471006b3593d1aba1
Adrian Gidaro <adriangidaro@gmail.com>
2024-11-20 20:47:49 +1100

    End of class

--- if_else.py
+++ if_else.py
@@ -1,4 +1,4 @@
-# is_raining = True
+is_raining = False
 
 # if is_raining:
 #     print('Bring Umbrella')
@@ -6,22 +6,87 @@
 #     print('Sunny Days')
 # print('The end!')
 
-# Else if
-# >80 -> HD
-# 70-79 -> D
-# 60-69 -> C
-# 50-59 -> P
-# <50 -> F
+# Ternary
+print('I need an umbrella' if is_raining else 'No need for an umbrella')
 
-marks = 51
+# # Else if
+# # >80 -> HD
+# # 70-79 -> D
+# # 60-69 -> C
+# # 50-59 -> P
+# # <50 -> F
 
-if marks >= 80:
-    print('HD')
-elif marks >= 70:
-    print('D')
-elif marks >= 60:
-    print('C')
-elif marks >= 50:
-    print('P')
-elif marks < 50:
-    print('F')
+# marks = 49
+
+# if marks >= 80:
+#     print('HD')
+# elif marks >= 70:
+#     print('D')
+# elif marks >= 60:
+#     print('C')
+# elif marks >= 50:
+#     print('P')
+# else:
+#     print('F')
+
+# Nested if
+
+# 2 states -> State1 and State2
+# State1: >= 18 can vote
+# State2: >= 21 can vote
+
+# age = 19
+# state = 'State2'
+
+# # Display whether they can vote or not
+
+# # if state == 'State1' and age >= 18:
+# #     print('Voting allowed')
+# # elif state == 'State2' and age >= 21:
+# #     print('Voting allowed')
+# # else:
+# #     print('Voting not allowed')
+
+# if state == 'State1':
+#     if age >= 18:
+#         print('Can vote')
+#     else:
+#         print('Cannot vote')
+# else:
+#     if age >= 21:
+#         print('Can vote')
+#     else:
+#         print('Cannot vote')
+
+# day_of_week = 3
+
+# # if day_of_week == 1:
+# #     print('Monday')
+# # elif day_of_week == 2:
+# #     print('Tuesday')
+
+# match day_of_week:
+#     case 1:
+#         print('Monday')
+#     case 2:
+#         print('Tuesday')
+#     case 3:
+#         print('Wednesday')
+#     case 4:
+#         print('Thursday')
+#     case 5:
+#         print('Friday')
+#     case _:
+#         print('That is not a weekday')
+        
+# # 1-5: Weekday
+# # 6, 7: Weekend
+# # Anything else: Error message
+
+# match day_of_week:
+#     case 1 | 2 | 3 | 4 | 5:
+#         print('Weekday')
+#     case 6 | 7:
+#         print('Weekend')
+#     case _:
+#         print('Error: not a valid day')
